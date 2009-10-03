da2746705c3a26fe7b6328bfecbf63c063f421cd
James Dennes <jdennes@gmail.com>
2009-10-03 15:09:32 +1000

    Added time to last.fm entries.

--- js/jd.js
+++ js/jd.js
@@ -1,7 +1,7 @@
 
 function tick() {
   var age = (new Date().getTime() - new Date().setTime(453900600000)) / 31536000000;
-  document.getElementById("age").innerHTML = age.toFixed(8);
+  $('span#age').html(age.toFixed(8));
 }
 
 function loadTweeting() {
@@ -9,7 +9,7 @@ function loadTweeting() {
   $('div#tweets').hide();
   getTwitters('tweets', { 
     id: 'jdennes', count: 6, enableLinks: true, ignoreReplies: true, clearContents: true,
-    template: '"%text%" <a class="when" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
+    template: '"%text%" <a class="when" href="http://twitter.com/%user_screen_name%/statuses/%id%/"><br />%time%</a>'
   });
   $('div#tweets').show();
   $('img#tweeting-loading').hide();
@@ -18,7 +18,7 @@ function loadTweeting() {
 function loadListening() {
   $('img#listening-loading').show();
   $('div#lastfm').hide();
-  $('div#lastfm').html('<dl><dt class="lfm_art"><a href="#"></a></dt><dd class="lfm_song"></dd><dd class="lfm_artist"></dd><dd class="lfm_album"></dd></dl>');
+  $('div#lastfm').html('<dl><dt class="lfm_art"><a href="#"></a></dt><dd class="lfm_song"></dd><dd class="lfm_artist"></dd><dd class="lfm_time"></dd></dl>');
   $('div#lastfm').lastFM({
     username: 'jdennes', apikey: '3ab5290b3f5973475ef258a41323fdad', number: 8, 
     artSize: 'small', noart: 'http://cdn.last.fm/flatness/catalogue/noimage/2/default_artist_small.png',
