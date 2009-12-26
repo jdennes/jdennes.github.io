03f6c99cb4a59db1edec37fd51f5577b5a18bb10
James Dennes <jdennes@gmail.com>
2009-12-27 10:58:04 +1100

    Added concatenation and minification of all javascript files using YUI Compressor.

--- js/flickr.js
+++ js/flickr.js
@@ -1,3 +1,4 @@
+
 (function($){
   $.fn.flickr = function(options) {
     var defaults = {
--- js/lastfm.js
+++ js/lastfm.js
@@ -1,13 +1,3 @@
-/*---------------
- * jQuery Last.Fm Plugin by Engage Interactive
- * Examples and documentation at: http://labs.engageinteractive.co.uk/lastfm/
- * Copyright (c) 2009 Engage Interactive
- * Version: 1.0 (10-JUN-2009)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- * Requires: jQuery v1.3 or later
----------------*/
 
 (function($){
   $.fn.lastFM = function(options) {
